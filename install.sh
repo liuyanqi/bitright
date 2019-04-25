24d8e8b5f0c379f83e20723958f4994fd25ff59f
Brandon <e0003810@u.nus.edu>
2019-04-25 15:13:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
-!/bin/bash
+#!/bin/bash
 
 pip install virtualenv
 virtualenv env
 cd env
 source bin/activate
-cd ..
-pip install -r requirements.txt
+pip install -r ../requirements.txt
 wget https://bitright.sfo2.digitaloceanspaces.com/tlsh.zip
 unzip tlsh.zip
 cd tlsh/py_ext
 python setup.py install
-cd ../../
+cd ../../..
